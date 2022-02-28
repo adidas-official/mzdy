0bca26ed4ccdc0ba6b5eb52528f9a191990e182a
adidas-official <blueman11235@gmail.com>
2022-02-28 18:55:15 +0100

    fixing stuff

--- amn.py
+++ amn.py
@@ -1,3 +1,14 @@
+# amn.py
+# -------------+
+# Automatizace |
+# Mzdovych     |
+# Nakladu      |
+# -------------+
+# v1
+# author: Zdenek Frydryn
+# created for: Bereko s.r.o., Drogerie Fiala s.r.o.,
+# Description: Automatizace vyplnovani mezd do xlsx tabulek pro urad prace.
+
 # TOOD:
 # 1) a) choose month; input num <= 12; month = input % 3 for UP
 #    b) month = months_cs[input - 1] for mzdove naklady
@@ -14,8 +25,10 @@
 import logging
 import csv
 import openpyxl
+import pyinputplus as pyin
 from pathlib import Path
 from openpyxl.utils import column_index_from_string
+from months_cz import months_cz
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
@@ -23,8 +36,12 @@ INPUT_FOLDER = Path.cwd() / 'INPUT'
 OUTPUT_FOLDER = Path.cwd() / 'OUTPUT'
 SRC_FOLDER = Path.cwd() / 'SRC'
 
-empl_data_csv_file = INPUT_FOLDER / 'I.CSV'
-with open(empl_data_csv_file, 'r') as empl_data_csv:
+BEREKO_empl_data_csv_file = INPUT_FOLDER / 'bereko.CSV'
+FIALA_empl_data_csv_file = INPUT_FOLDER / 'fiala.CSV'
+
+# MONTH = pyin.inputNum('Choose month [1-12]\n', min=1, max=12)
+
+with open(BEREKO_empl_data_csv_file, 'r') as empl_data_csv:
     empl_data_reader = csv.reader(empl_data_csv)
     empl_data = list(empl_data_reader)
     empl_data = empl_data[1:]
@@ -84,82 +101,124 @@ for i in empl_data:
 
 # open mzdy UP table, go through each name in data and check if it is in table
 wb = openpyxl.load_workbook(SRC_FOLDER / 'jmenny_seznam_2021_10_01 Bereko.xlsx')
-ws = wb['2) jmenný seznam']
+sheets = ['2) jmenný seznam', '3) nákl. prov. z. a prac. a.']
+# ws = wb['2) jmenný seznam']
 col_letter_id = column_index_from_string('D')
-col_letter_pay = column_index_from_string('K')
+# if MONTH % 3 == 1:
+#     col_letter_pay_sheet2 = 11
+#     col_letter_pay_sheet3 = 9
+# elif MONTH % 3 == 2:
+#     col_letter_pay_sheet2 = 16
+#     col_letter_pay_sheet3 = 10
+# else:
+#     col_letter_pay_sheet2 = 21
+#     col_letter_pay_sheet3 = 11
 
+col_letter_pay_sheet2 = 11
+col_letter_pay_sheet3 = 9
 ids_in_xlsx = {}
-last_row = 13
+last_rows = []
 
-for i in range(10):
-    row = i + 13
-    id_of_person = ws.cell(row=row, column=col_letter_id).value
-    # make list of ids in xlsx file
-    if id_of_person:
-        ids_in_xlsx.setdefault(id_of_person, {'row': row})
-        last_row = row
+for sheet in sheets:
+    ws = wb[sheet]
+    last_row = 13
+    ids_in_xlsx.setdefault(sheet, {})
+
+    for i in range(10):
+        row = i + 13
+        id_of_person = ws.cell(row=row, column=col_letter_id).value
+        # make list of ids in xlsx file
+        if id_of_person:
+            ids_in_xlsx[sheet].setdefault(id_of_person.replace('/', ''), {'row': row})
+            last_row = row
+
+    last_rows.append(last_row)
+
+logging.info(ids_in_xlsx)
+
+# ws = wb['3) nákl. prov. z. a prac. a.']
+# for i in range(10):
+#     row = i + 14
+#     id_of_person = ws.cell(row=row, column=col_letter_id).value
+#     # make list of ids in xlsx file
+#     if id_of_person:
+#         formated_id = id_of_person.replace('/', '')
+#         ids_in_xlsx.setdefault(formated_id, {'row': row})
+#         last_row_asist = row
+
+ws = wb['2) jmenný seznam']
 
 in_xlsx_only = {}
 in_xlsx_and_csv_data = {}
 in_csv_only = {}
 
 for i in employees_up:
-    if i not in ids_in_xlsx:
-        # new employee or asistenti sheet
-        in_csv_only[i] = employees_up[i]
-    else:
-        # grab cell coord and add it to dict in_xlsx_and_csv_data
-        in_xlsx_and_csv_data[i] = employees_up[i]
-        in_xlsx_and_csv_data[i]['row'] = ids_in_xlsx[i]['row']
+    for sheet in ids_in_xlsx:
+        # ================================================================================================= END
+        if i not in sheet:
+            # new employee or asistenti sheet
+            in_csv_only[i] = employees_up[i]
+        else:
+            # grab cell coord and add it to dict in_xlsx_and_csv_data
+            in_xlsx_and_csv_data[i] = employees_up[i]
+            in_xlsx_and_csv_data[i]['row'] = ids_in_xlsx[i]['row']
 
+logging.info(in_xlsx_and_csv_data)
 # person located in xlsx and is in csv
 # insert payment expences to each employee that is in table and is
 for person in in_xlsx_and_csv_data:
-    # logging.info(in_xlsx_and_csv_data[person]['row'])
     row = in_xlsx_and_csv_data[person]['row']
-    ws.cell(row=row, column=column_index_from_string('K')).value = in_xlsx_and_csv_data[person]['payment expences']
+    ws.cell(row=row, column=col_letter_pay_sheet2).value = in_xlsx_and_csv_data[person]['payment expences']
 
 
 # person located in xlsx and is NOT in the csv
 for i in ids_in_xlsx:
     if i not in employees_up:
-        ws.cell(row=ids_in_xlsx[i]['row'], column=column_index_from_string('J')).value = ''
+        ws.cell(row=ids_in_xlsx[i]['row'], column=column_index_from_string('J')).value = ''  # check sheet!
 
+# logging.info(in_csv_only)
 
 # person NOT located in xlsx and is in the csv => new employee
-if len(in_csv_only) > 0:
-    ws = wb['3) nákl. prov. z. a prac. a.']
-    for person in in_csv_only:
-        # logging.info(in_csv_only[person])
-        person_id = person[:6] + '/' + person[6:]
-        located = False
-        last_row_asist = 14
-        for i in range(last_row_asist, 20):
-            rc = ws.cell(row=i, column=column_index_from_string('D')).value
-            if rc == person_id:
-                ws.cell(row=i, column=column_index_from_string('I')).value = in_csv_only[person]['payment expences']
-                located = True
-                last_row_asist = i
-        if not located:
-            if in_csv_only[person]['cat'] == 'INV':
-                ws = wb['2) jmenný seznam']
-                ws.cell(row=last_row+1, column=column_index_from_string('B')).value = in_csv_only[person]['last name']
-                ws.cell(row=last_row+1, column=column_index_from_string('C')).value = in_csv_only[person]['first name']
-                ws.cell(row=last_row+1, column=column_index_from_string('D')).value = person
-                ws.cell(row=last_row+1, column=column_index_from_string('G')).value = in_csv_only[person]['ins code']
-                ws.cell(row=last_row+1, column=column_index_from_string('K')).value = in_csv_only[person]['payment expences']
-            else:
-                ws = wb['3) nákl. prov. z. a prac. a.']
-                ws.cell(row=last_row_asist+1, column=column_index_from_string('B')).value = in_csv_only[person]['last name']
-                ws.cell(row=last_row_asist+1, column=column_index_from_string('C')).value = in_csv_only[person]['first name']
-                ws.cell(row=last_row_asist+1, column=column_index_from_string('D')).value = person_id
-                ws.cell(row=last_row_asist+1, column=column_index_from_string('E')).value = '-\'\'-'
-                ws.cell(row=last_row_asist+1, column=column_index_from_string('F')).value = 'PA'
-                ws.cell(row=last_row_asist+1, column=column_index_from_string('G')).value = '100%'
-                ws.cell(row=last_row_asist+1, column=column_index_from_string('I')).value = in_csv_only[person]['payment expences']
+# if len(in_csv_only) > 0:
+#     ws = wb['3) nákl. prov. z. a prac. a.']
+#     for person in in_csv_only:
+#         # logging.info(in_csv_only[person])
+#         person_id = person[:6] + '/' + person[6:]
+#         located = False
+#         # logging.info(last_row_asist)
+#         for i in range(last_row_asist, 20):
+#             rc = ws.cell(row=i, column=column_index_from_string('D')).value
+#             if rc:
+#                 if '/' not in rc:
+#                     rc = rc[:6] + '/' + rc[6:]
+#                 logging.info('rc:' + str(rc))
+#                 logging.info('person id:' + str(person_id))
+#                 if rc == person_id:
+#                     logging.info('here')
+#                     ws.cell(row=i, column=column_index_from_string('I')).value = in_csv_only[person]['payment expences']
+#                     located = True
+#                     last_row_asist = i
+#                 if not located:
+#                     if in_csv_only[person]['cat'] == 'INV':
+#                         ws = wb['2) jmenný seznam']
+#                         ws.cell(row=last_row+1, column=column_index_from_string('B')).value = in_csv_only[person]['last name']
+#                         ws.cell(row=last_row+1, column=column_index_from_string('C')).value = in_csv_only[person]['first name']
+#                         ws.cell(row=last_row+1, column=column_index_from_string('D')).value = person
+#                         ws.cell(row=last_row+1, column=column_index_from_string('G')).value = in_csv_only[person]['ins code']
+#                         ws.cell(row=last_row+1, column=col_letter_pay_sheet2).value = in_csv_only[person]['payment expences']
+#                     else:
+#                         ws = wb['3) nákl. prov. z. a prac. a.']
+#                         ws.cell(row=last_row_asist+1, column=column_index_from_string('B')).value = in_csv_only[person]['last name']
+#                         ws.cell(row=last_row_asist+1, column=column_index_from_string('C')).value = in_csv_only[person]['first name']
+#                         ws.cell(row=last_row_asist+1, column=column_index_from_string('D')).value = person_id
+#                         ws.cell(row=last_row_asist+1, column=column_index_from_string('E')).value = '-\'\'-'
+#                         ws.cell(row=last_row_asist+1, column=column_index_from_string('F')).value = 'PA'
+#                         ws.cell(row=last_row_asist+1, column=column_index_from_string('G')).value = '100%'
+#                         ws.cell(row=last_row_asist+1, column=col_letter_pay_sheet3).value = in_csv_only[person]['payment expences']
+#                         # last_row_asist += 1
 
 # save as tempcopy-up-fiala.xlsx
-wb.save(OUTPUT_FOLDER / 'temp.xlsx')
+# wb.save(OUTPUT_FOLDER / 'temp.xlsx')
 # open document for visual check
 
 # open mzdove naklady fiala, go through each sheet and check if name is in data
